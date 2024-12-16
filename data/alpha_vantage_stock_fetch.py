e02ccae53448147d1740203e953c19c133892b5f
Dan-Gold <dan.goldschmidt96@gmail.com>
2024-12-15 21:35:19 -0500

    other files

---
+++ data/alpha_vantage_stock_fetch.py
@@ -0,0 +1,31 @@
+import requests
+import pandas as pd
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+API_KEY = os.getenv("ALPHAVANTAGE_API_KEY")
+SYMBOL = "PLTR"
+INTERVAL = "1min"
+
+url = f"https://www.alphavantage.co/query"
+params = {
+    "function": "TIME_SERIES_INTRADAY",
+    "symbol": SYMBOL,
+    "interval": INTERVAL,
+    "apikey": API_KEY,
+    "outputsize": "full",  # Fetch maximum data available
+    "datatype": "csv"
+}
+response = requests.get(url, params=params)
+
+file_name = f"{SYMBOL}_data_{INTERVAL}_extra_1.csv"
+
+# Save to CSV
+with open(file_name, "w") as file:
+    file.write(response.text)
+
+# Load into DataFrame
+df = pd.read_csv(file_name)
+print(df.head())
